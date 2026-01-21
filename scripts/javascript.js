a1baceac05f4b0b9f964a3ed46fa2f2837a013a2
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-01-21 13:31:34 -0500

    Write functions for creating and storing books

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,2 +1,14 @@
-const dialog = document.querySelector(".add-book-dialog");
-dialog.showModal();
+const myLibrary = [];
+
+function Book(title, author, numPages, hasRead) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.hasRead = hasRead;
+}
+
+function addBookToLibrary(title, author, numPages, hasRead) {
+    const newBook = new Book(title, author, numPages, hasRead);
+    myLibrary.push(newBook);
+}
