192b45e5f9a6797cc1f0e764570c18b7743d2333
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-01-23 11:48:57 -0500

    Add the ability to add new books through a togglable form

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,7 +1,7 @@
 const myLibrary = [];
 
 for(let i = 0; i < 12; i++) {
-    addBookToLibrary("Title", "Author", `${360 + i} pages`, true)
+    addBookToLibrary("Title", "Author", 360 + i, true)
 }
 
 function Book(title, author, pages, hasRead) {
@@ -29,7 +29,7 @@ Book.prototype.display = function() {
 
     const pages = document.createElement("div");
     pages.className = "pages";
-    pages.textContent = this.pages;
+    pages.textContent = `${this.pages} pages`;
 
     const readStatusBtn = document.createElement("button");
     // The "has-read" and "has-not-read" classes each set corresponding background images
@@ -38,7 +38,7 @@ Book.prototype.display = function() {
     // The callback function must be bound to "this" in the current context.
     // Otherwise, when the function is called, the context will change and
     // "this" will be set to the button that triggered the function rather than 
-    // the object that the function originated from.
+    // the object that the function originated from
     readStatusBtn.addEventListener("click", this.toggleReadStatus.bind(this));
 
     const removeBtn = document.createElement("button");
@@ -62,9 +62,35 @@ Book.prototype.toggleReadStatus = function(event) {
     event.target.className = `book-btn read-status-btn ${this.hasRead ? "has-read" : "has-not-read"}`;
 }
 
+myLibrary.forEach(book => book.display());
+
+const addBookDialog = document.querySelector("#add-book-dialog");
+const addBookForm = document.querySelector("#add-book-form");
+const bookFormBtn = document.querySelector("#book-form-btn");
+bookFormBtn.addEventListener("click", () => {
+    addBookForm.reset();
+    addBookDialog.showModal();
+});
+
+const submitBookBtn = document.querySelector("#submit-book-btn");
+submitBookBtn.addEventListener("click", (event) => {
+    const title = document.querySelector("#title");
+    const author = document.querySelector("#author");
+    const pages = document.querySelector("#pages");
+    const hasRead = document.querySelector("#read-status");
+
+    const allFieldsFilledOut = title.value && author.value && pages.value;
+    
+    if(allFieldsFilledOut) {
+        addBookToLibrary(title.value, author.value, pages.value, hasRead.checked);
+        myLibrary.at(-1).display();  
+    }
+});
+
+const closeFormBtn = document.querySelector("#close-form-btn");
+closeFormBtn.addEventListener("click", () => addBookDialog.close());
+
 function addBookToLibrary(title, author, pages, hasRead) {
     const newBook = new Book(title, author, pages, hasRead);
     myLibrary.push(newBook);
 }
-
-myLibrary.forEach(book => book.display());
