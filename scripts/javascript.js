31b62d8a8021aa178a7d2f36f48ad7f62b038d8d
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-04-08 10:41:10 -0400

    Improve add book form validation

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,199 +1,225 @@
 const myLibrary = [];
 
-// Storing the width of each book in an array provides a convenient way to access the current 
+// Storing the width of each book in an array provides a convenient way to access the current
 // widest and second widest books, which allows for the book grid columns to be resized to
 // the second widest book's width when the first widest book is removed.
 const bookWidths = [];
 
 const root = document.querySelector(":root");
 const minGridColumnSize = +getComputedStyle(root)
-                          .getPropertyValue("--grid-column-size")
-                          .slice(0, -2);
-let currentGridColumnSize = minGridColumnSize; 
+  .getPropertyValue("--grid-column-size")
+  .slice(0, -2);
+let currentGridColumnSize = minGridColumnSize;
 
 class Book {
-    #id;
-    #title;
-    #author;
-    #pages;
-    #hasRead;
+  #id;
+  #title;
+  #author;
+  #pages;
+  #hasRead;
 
-    constructor(title, author, pages, hasRead) {
-        this.#id = crypto.randomUUID();
-        this.#title = title;
-        this.#author = author;
-        this.#pages = pages;
-        this.#hasRead = hasRead;
+  constructor(title, author, pages, hasRead) {
+    this.#id = crypto.randomUUID();
+    this.#title = title;
+    this.#author = author;
+    this.#pages = pages;
+    this.#hasRead = hasRead;
+  }
+
+  display() {
+    const bookContainer = document.querySelector("#book-container");
+
+    const book = document.createElement("div");
+    book.className = "book";
+    book.dataset.id = this.#id;
+
+    const title = document.createElement("div");
+    title.className = "title";
+    title.textContent = this.#title;
+
+    const author = document.createElement("div");
+    author.className = "author";
+    author.textContent = this.#author;
+
+    const pages = document.createElement("div");
+    pages.className = "pages";
+    pages.textContent = `${this.#pages} pages`;
+
+    const readStatusBtn = document.createElement("button");
+    // The "has-read" and "has-not-read" classes each set corresponding background images
+    // for the button, which are toggled accordingly.
+    readStatusBtn.className = `book-btn read-status-btn ${this.#hasRead ? "has-read" : "has-not-read"}`;
+    // The callback function must be bound to "this" in the current context.
+    // Otherwise, when the function is called, the context will change and
+    // "this" will be set to the button that triggered the function rather than
+    // the object that the function originated from.
+    readStatusBtn.addEventListener("click", this.toggleReadStatus.bind(this));
+
+    const removeBtn = document.createElement("button");
+    removeBtn.className = "book-btn remove-btn";
+
+    removeBtn.addEventListener("click", (event) => {
+      const removedBookDisplay = event.target.parentNode;
+      bookContainer.removeChild(removedBookDisplay);
+
+      const removedBookObj = myLibrary.splice(
+        myLibrary.findIndex(
+          (book) => book.#id === removedBookDisplay.dataset.id,
+        ),
+        1,
+      )[0];
+
+      // Each entry in the book widths array is an object containing both the width of a book,
+      // and the object associated with that book. Without this association, it would not
+      // be possible to retrieve the width of individual books.
+      const removedBookWidth = bookWidths.find(
+        (bookWidth) => bookWidth.book === removedBookObj,
+      ).value;
+
+      // When the widest book is removed, the grid columns will be resized to the width of
+      // the second widest book. This ensures that books will not take up more space
+      // than they need to while still maintaining uniformity, making for a more natural
+      // appearance.
+      const modifyingGridColumnSize =
+        removedBookWidth === currentGridColumnSize &&
+        currentGridColumnSize > minGridColumnSize;
+      if (modifyingGridColumnSize) {
+        // Sorting the book widths array provides both a convenient way to remove
+        // the largest width, and a convenient way to retrieve the width of the
+        // second widest book.
+        bookWidths.sort(
+          (bookWidth1, bookWidth2) => bookWidth1.value - bookWidth2.value,
+        );
+        bookWidths.pop();
+        currentGridColumnSize = bookWidths.at(-1).value;
+        root.style.setProperty(
+          "--grid-column-size",
+          currentGridColumnSize + "px",
+        );
+      } else {
+        bookWidths.splice(
+          bookWidths.findIndex(
+            (bookWidth) => bookWidth.value === removedBookWidth,
+          ),
+          1,
+        );
+      }
+    });
+
+    book.append(title, author, pages, readStatusBtn, removeBtn);
+
+    bookContainer.appendChild(book);
+
+    // If the intrinsic width of the book is larger than the current grid column size,
+    // then the grid columns will be resized to that width. This is to ensure that
+    // there is no content overflow while maintaining uniform grid column sizing.
+    const bookWidth = +getComputedStyle(book)
+      .getPropertyValue("width")
+      .slice(0, -2);
+
+    if (bookWidth > currentGridColumnSize) {
+      currentGridColumnSize = bookWidth;
+      root.style.setProperty(
+        "--grid-column-size",
+        currentGridColumnSize + "px",
+      );
     }
 
-    display() {
-        const bookContainer = document.querySelector("#book-container");
+    bookWidths.push(new BookWidth(this, bookWidth));
 
-        const book = document.createElement("div");
-        book.className = "book";
-        book.dataset.id = this.#id;
+    // Books are initially absolutely positioned so that they ignore the grid content flow
+    // and maintain their intrinsic width, which is needed for the above calculations.
+    // Once this width has been obtained and the new grid column size (if any) has been
+    // calculated, this class will change its positioning to relative so that it is resized
+    // and slotted into the grid accordingly.
+    book.className += " relatively-positioned";
+  }
 
-        const title = document.createElement("div");
-        title.className = "title"
-        title.textContent = this.#title;
-
-        const author = document.createElement("div");
-        author.className = "author";
-        author.textContent = this.#author;
-
-        const pages = document.createElement("div");
-        pages.className = "pages";
-        pages.textContent = `${this.#pages} pages`;
-
-        const readStatusBtn = document.createElement("button");
-        // The "has-read" and "has-not-read" classes each set corresponding background images
-        // for the button, which are toggled accordingly.
-        readStatusBtn.className = `book-btn read-status-btn ${this.#hasRead ? "has-read" : "has-not-read"}`;
-        // The callback function must be bound to "this" in the current context.
-        // Otherwise, when the function is called, the context will change and
-        // "this" will be set to the button that triggered the function rather than 
-        // the object that the function originated from.
-        readStatusBtn.addEventListener("click", this.toggleReadStatus.bind(this));
-
-        const removeBtn = document.createElement("button");
-        removeBtn.className = "book-btn remove-btn";
-
-        removeBtn.addEventListener("click", (event) => {
-            const removedBookDisplay = event.target.parentNode;
-            bookContainer.removeChild(removedBookDisplay);
-
-            const removedBookObj = myLibrary.splice(myLibrary.findIndex
-                                            (book => book.#id === removedBookDisplay.dataset.id), 1)[0];
-
-            // Each entry in the book widths array is an object containing both the width of a book,
-            // and the object associated with that book. Without this association, it would not
-            // be possible to retrieve the width of individual books.
-            const removedBookWidth = bookWidths
-                                    .find((bookWidth) => bookWidth.book === removedBookObj)
-                                    .value;
-
-            // When the widest book is removed, the grid columns will be resized to the width of
-            // the second widest book. This ensures that books will not take up more space
-            // than they need to while still maintaining uniformity, making for a more natural
-            // appearance. 
-            const modifyingGridColumnSize = removedBookWidth === currentGridColumnSize && 
-                                            currentGridColumnSize > minGridColumnSize;
-            if(modifyingGridColumnSize) {
-                // Sorting the book widths array provides both a convenient way to remove 
-                // the largest width, and a convenient way to retrieve the width of the 
-                // second widest book.
-                bookWidths.sort((bookWidth1, bookWidth2) => bookWidth1.value - bookWidth2.value);
-                bookWidths.pop();
-                currentGridColumnSize = bookWidths.at(-1).value;
-                root.style.setProperty("--grid-column-size", currentGridColumnSize + "px");
-            } else {
-                bookWidths.splice(bookWidths.findIndex
-                        (bookWidth => bookWidth.value === removedBookWidth), 1);
-            }
-        });
-
-        book.append(title, author, pages, readStatusBtn, removeBtn);
-
-        bookContainer.appendChild(book);
-
-        // If the intrinsic width of the book is larger than the current grid column size, 
-        // then the grid columns will be resized to that width. This is to ensure that
-        // there is no content overflow while maintaining uniform grid column sizing.
-        const bookWidth = +getComputedStyle(book).getPropertyValue("width").slice(0, -2);
-
-        if(bookWidth > currentGridColumnSize) {
-            currentGridColumnSize = bookWidth;
-            root.style.setProperty("--grid-column-size", currentGridColumnSize + "px");
-        }
-
-        bookWidths.push(new BookWidth(this, bookWidth));
-
-        // Books are initially absolutely positioned so that they ignore the grid content flow
-        // and maintain their intrinsic width, which is needed for the above calculations. 
-        // Once this width has been obtained and the new grid column size (if any) has been 
-        // calculated, this class will change its positioning to relative so that it is resized
-        // and slotted into the grid accordingly.
-        book.className += " relatively-positioned";
-    }
-
-    toggleReadStatus(event) {
-        this.#hasRead = !this.#hasRead;
-        // The "has-read" and "has-not-read" classes each set corresponding background images
-        // for the button, which are toggled accordingly.
-        event.target.className = `book-btn read-status-btn ${this.#hasRead ? "has-read" : "has-not-read"}`;
-    }
+  toggleReadStatus(event) {
+    this.#hasRead = !this.#hasRead;
+    // The "has-read" and "has-not-read" classes each set corresponding background images
+    // for the button, which are toggled accordingly.
+    event.target.className = `book-btn read-status-btn ${this.#hasRead ? "has-read" : "has-not-read"}`;
+  }
 }
 
 class BookWidth {
-    #book;
-    #value;
-    
-    constructor(book, value) {
-        this.#book = book;
-        this.#value = value; 
-    }
+  #book;
+  #value;
 
-    get book() {
-        return this.#book;
-    }
+  constructor(book, value) {
+    this.#book = book;
+    this.#value = value;
+  }
 
-    get value() {
-        return this.#value;
-    }
+  get book() {
+    return this.#book;
+  }
+
+  get value() {
+    return this.#value;
+  }
 }
 
 const addBookDialog = document.querySelector("#add-book-dialog");
 const addBookForm = document.querySelector("#add-book-form");
 const bookFormBtn = document.querySelector("#book-form-btn");
-const validationFailedMessage = document.querySelector("#validation-failed-message");
+const validationFailedMessage = document.querySelector(
+  "#validation-failed-message",
+);
 
 bookFormBtn.addEventListener("click", () => {
-    addBookForm.reset();
-    validationFailedMessage.className = "validation-failed-message hidden";
-    addBookDialog.showModal();
+  addBookForm.reset();
+  validationFailedMessage.className = "validation-failed-message hidden";
+  addBookDialog.showModal();
 });
 
 const submitBookBtn = document.querySelector("#submit-book-btn");
-submitBookBtn.addEventListener("click", (event) => {
-    let title = document.querySelector("#title").value;
-    let author = document.querySelector("#author").value;
-    let pages = document.querySelector("#pages").value;
-    let hasRead = document.querySelector("#read-status").checked;
+submitBookBtn.addEventListener("click", () => {
+  const title = document.querySelector("#title");
+  const author = document.querySelector("#author");
+  const pages = document.querySelector("#pages");
+  const hasRead = document.querySelector("#read-status");
 
-    // Validation checks: all fields should be filled out, author name can only contain
-    // letters/single spaces, and the max page number limit is 9999.
-    const allFieldsFilledOut = title && author && pages;
-    const validAuthor = /^([a-zA-Z]+\s)*[a-zA-Z]+$/.test(author);
-    if (pages > 9999) pages = 9999;
-    
-    if(allFieldsFilledOut && validAuthor) {
-        // Capitalize both the title and the author, if they aren't capitalized already.
-        title = title.charAt(0).toUpperCase() + title.slice(1);
+  if (addBookForm.checkValidity()) {
+    // Capitalize both the title and the author, if they aren't capitalized already.
+    const titleValue =
+      title.value.charAt(0).toUpperCase() + title.value.slice(1);
 
-        author = author
-                .split(" ")
-                .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-                .join(" ");
+    const authorValue = author.value
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
 
-        addBookToLibrary(title, author, pages, hasRead);
-        myLibrary.at(-1).display();  
+    // Max page number limit is 9999.
+    const pagesValue = pages.value > 9999 ? 9999 : pages.value;
 
-        addBookDialog.close();
-    } else {
-        validationFailedMessage.textContent = !allFieldsFilledOut ?
-                                              "Please fill out missing fields" :
-                                              "Author name can only include letters and single spaces";
+    const hasReadValue = hasRead.value;
 
-        validationFailedMessage.className = "validation-failed-message visible";
-    }
+    addBookToLibrary(titleValue, authorValue, pagesValue, hasReadValue);
+    myLibrary.at(-1).display();
+
+    addBookDialog.close();
+  } else {
+    const missingFields =
+      title.validity.valueMissing &&
+      author.validity.valueMissing &&
+      pages.validity.valueMissing;
+
+    validationFailedMessage.textContent = missingFields
+      ? "Please fill out missing fields"
+      : "Author name can only include letters and single spaces";
+      
+    validationFailedMessage.className = "validation-failed-message visible";
+  }
 });
 
 const closeFormBtn = document.querySelector("#close-form-btn");
 closeFormBtn.addEventListener("click", () => addBookDialog.close());
 
 function addBookToLibrary(title, author, pages, hasRead) {
-    const newBook = new Book(title, author, pages, hasRead);
-    myLibrary.push(newBook);
+  const newBook = new Book(title, author, pages, hasRead);
+  myLibrary.push(newBook);
 }
 
 addBookToLibrary("No Longer Human", "Osamu Dazai", 176, true);
@@ -207,4 +233,4 @@ addBookToLibrary("American Psycho", "Bret Easton Ellis", 399, false);
 addBookToLibrary("Flowers for Algernon", "Daniel Keyes", 311, true);
 addBookToLibrary("Life of the Party", "Olivia Gatwood", 176, true);
 
-myLibrary.forEach(book => book.display());
+myLibrary.forEach((book) => book.display());
